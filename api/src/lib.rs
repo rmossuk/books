772f1c165a71ebdb94b5aa45a637409dea4d5bb7
richardmoss <richardm@airblackbox.com>
2023-02-15 20:57:54 +0000

    commit4

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,4 +1,3 @@
-#![allow(dead_code)]
 use anyhow::Result;
 use spin_sdk::{
     http::{Request, Response},
@@ -8,12 +7,13 @@ use spin_sdk::{
 use serde_json;
 use serde::{Deserialize, Serialize};
 
-// The environment variable set in `spin.toml` that points to the
-// address of the Pg server that the component will write to
+// set which ENV VAR to use.   &str is a reference to a string literal.
 const DB_URL_ENV: &str = "DB_URL";
 
+// The Book struct is annotated with the Serialize and Deserialize attributes, which are used to indicate that this struct can be serialized and deserialized.
+// The struct is also annotated with the Debug and Clone attributes, which are used to indicate that this struct can be printed and cloned.
+// the option type is used to indicate that the field is optional.
 #[derive(Debug, Clone, Serialize, Deserialize)]
-
 struct Book {
     id: i32,
     title: String,
@@ -30,48 +30,27 @@ struct Book {
     tag: Option<String>,
 }
 
-// impl TryFrom<&pg::Row> for Article {
-//     type Error = anyhow::Error;
-
-//     fn try_from(row: &pg::Row) -> Result<Self, Self::Error> {
-//         let id = i32::decode(&row[0])?;
-//         let title = String::decode(&row[1])?;
-//         let content = String::decode(&row[2])?;
-//         let authorname = String::decode(&row[3])?;
-//         let coauthor = Option::<String>::decode(&row[4])?;
-
-//         Ok(Self {
-//             id,
-//             title,
-//             content,
-//             authorname,
-//             coauthor,
-//         })
-//     }
-// }
-
+// The function is annotated with the http_component macro, which is used to indicate that this is a handler for an HTTP request.
 #[http_component]
-fn process(req: Request) -> Result<Response> {
-    println!("path: {}", req.headers().get("spin-full-url").unwrap().to_str().unwrap());
-    
-    match req.uri().path() {
-        "/api/" => books(req),
-        // "/book" => book(req),
-        _ => Ok(http::Response::builder()
-            .status(404)
-            .body(Some("Not found".into()))?),
-    }
-}
-
-fn books(_req: Request) -> Result<Response> {
+fn api(_req: Request) -> Result<Response> {
+    // get the database address from the environment variable
     let address = std::env::var(DB_URL_ENV)?;
 
+    // query the database and return the results as a rowset.
+    // a rowset is a collection of rows, where each row is a collection of columns.
     let sql = "SELECT * FROM books";
     let rowset = pg::query(&address, sql, &[])?;
 
-    // using the rowset, return a list of articles in json format and return using ok http response builder
+    // create an empty vector of books
+    // a vector is a collection of items of the same type.
     let mut books: Vec<Book> = Vec::new();
 
+    // iterate over the rows in the rowset
+    // each row is a collection of columns
+    // each column is a collection of bytes
+    // the decode function is used to convert the bytes into a value of the specified type
+    // the option type is used to indicate that the field is optional.
+    // the ? operator is used to return an error if the decode function fails.
     for row in rowset.rows {
         let book = Book {
             id: i32::decode(&row[0])?,
@@ -88,48 +67,19 @@ fn books(_req: Request) -> Result<Response> {
             image: Option::<String>::decode(&row[11])?,
             tag: Option::<String>::decode(&row[12])?,
         };
+        // add the book to the vector of books
         books.push(book);
     }
 
+    // convert the vector of books to a JSON string
+    // the ? operator is used to return an error if the conversion fails.
     let json = serde_json::to_string(&books)?;
+
+    // return the JSON string as the body of the HTTP response
+    // the some function is used to indicate that the body is not empty.
+    // the into function is used to convert the string into a byte array.
     Ok(http::Response::builder()
         .status(200)
         .header("Content-Type", "application/json")
         .body(Some(json.into()))?)
 }
-
-// fn book(_req: Request) -> Result<Response> {
-//     let address = std::env::var(DB_URL_ENV)?;
-
-//     let id: &str = _req
-//         .headers()
-//         .get("spin-path-info")
-//         .unwrap()
-//         .to_str()
-//         .unwrap()
-//         .strip_prefix('/')
-//         .unwrap();
-
-    
-//     println!("id: {}", id);
-
-//     let sql = format!("SELECT id, title, content, authorname, coauthor FROM articletest WHERE id = {}", id);
-    
-//     println!("sql: {}", sql);
-
-//     let rowset = pg::query(&address, sql.as_str(), &[])?;
-
-    
-//     // using the rowset, return the article in json format and return using ok http response builder
-//     if let Some(row) = rowset.rows.get(0) {
-//         let article = Article::try_from(row)?;
-//         let json = serde_json::to_string(&article)?;
-//         Ok(http::Response::builder()
-//             .status(200)
-//             .body(Some(json.into()))?)
-//     } else {
-//         Ok(http::Response::builder()
-//             .status(404)
-//             .body(Some("Book not found".into()))?)
-//     }
-// }
--- src/App.js
+++ src/App.js
@@ -6,23 +6,54 @@ import Toolbar from '@mui/material/Toolbar';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
-import { maxWidth } from '@mui/system';
+
+function Book({ book, index }) {
+  return (
+    <div className={"booksContainer"+index}>
+      <Container maxWidth="md" className="booksInner">
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+          <div className="bookHeader">
+            <h2>{book.title}</h2>
+          </div>
+          <div className="bookContent">
+            <p>{book.subtitle}</p>
+            <p>{book.summary}</p>
+            <p>{book.content}</p>
+          </div>
+          <div className="bookFooter">
+            <p>
+              <Link href={book.audible} target="_blank">Audible</Link> 
+              <Link href={book.paperback} target="_blank">Paperback</Link> 
+              <Link href={book.kindle} target="_blank">Kindle</Link> 
+              <Link href={book.ebook} target="_blank">ebook</Link>
+            </p>
+          </div>
+          </Grid>
+          <Grid item xs={4}>
+          <img src={book.image}/>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+}
 
 function App() {
 
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const fetchData = () => {
-    fetch("http://127.0.0.1:3000/api/")
-    .then((response) => response.json())
-    .then((response) => {
-      setBooks(response);
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:3000/api/");
+      const data = await response.json();
+      setBooks(data);
       setLoading(false);
-    })
-    .catch(() => {
-    });
-  }
+    } catch (error) {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     fetchData()
@@ -33,56 +64,22 @@ function App() {
     return <div>Loading...</div>;
   }
 
-
-
   return (
     <div className="App">
       <Box sx={{ flexGrow: 1 }}>
       <AppBar component="nav" className='nigelHeader' >
         <Toolbar>
-          <img
-          src="https://i0.wp.com/nigelpoulton.com/wp-content/uploads/2022/11/nigelpoulton_logo_22_colour.png?fit=300%2C60&ssl=1"
-          alt=""
-          loading="lazy"
-        />
-         
+          <img src="https://i0.wp.com/nigelpoulton.com/wp-content/uploads/2022/11/nigelpoulton_logo_22_colour.png?fit=300%2C60&ssl=1"/> 
         </Toolbar>
       </AppBar>
     </Box>
 
     <div class="main">
       {books.map((book, index) => (
-        <div className={"booksContainer"+index}>
-          <Container maxWidth="md" className="booksInner">
-            <Grid container spacing={2}>
-              <Grid item xs={8}>
-              <div className="bookHeader">
-                <h2>{book.title}</h2>
-              </div>
-              <div className="bookContent">
-                <p>{book.subtitle}</p>
-                <p>{book.summary}</p>
-                <p>{book.content}</p>
-              </div>
-              <div className="bookFooter">
-                <p>
-                  <Link href={book.audible}>Audible</Link> 
-                  <Link href={book.paperback}>Paperback</Link> 
-                  <Link href={book.kindle}>Kindle</Link> 
-                  <Link href={book.ebook}>ebook</Link>
-                </p>
-              </div>
-              </Grid>
-              <Grid item xs={4}>
-              <img src={book.image}/>
-              </Grid>
-            </Grid>
-          </Container>
-        </div>
+        <Book key={index} index={index} book={book} />
       ))}
       </div>
   </div>
-
   );
 }
 
